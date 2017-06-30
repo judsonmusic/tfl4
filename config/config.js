c484b330e5faacc86a62711dbf26d562f0501381
jterrell <jterrell@drfirst.com>
2017-06-30 09:46:16 -0400

    changes

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
 
     'secret': 'tfl',
-    //'database': 'mongodb://tfl:TrainForLifeDev1@ec2-54-172-121-24.compute-1.amazonaws.com/tfl',
-    'database': 'mongodb://tfl:TrainForLifeDev1@192.168.1.16/tfl'
+    'database': 'mongodb://tfl:TrainForLifeDev1@judsondesigns.dyndns.org:27017/tfl'
+    //'database': 'mongodb://tfl:TrainForLifeDev1@192.168.1.16/tfl'
 
 };
