77afce87bab4d024df890908a3212b14c802230d
Judson Terrell <judsonterrell@Judsons-Mac-Pro.local>
2018-05-22 11:12:49 -0400

    changes:

--- server.js
+++ server.js
@@ -428,7 +428,7 @@ router.route('/accounts')
           //console.log('WE FOUND AN EMAIL!!');
 
           res.json({
-            message: 'Account already exists. To log back in using this account please go to the home page.',
+            message: 'Account already exists. If this email address belongs to you, please click the home link and try logging in. If you have forgotten your password, please click the forgot password link on the home page.',
             success: false
           });
           res.end();
